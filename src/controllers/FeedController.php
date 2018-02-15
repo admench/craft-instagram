f28fe707c42b75c56e59081ca7d35790ce29bd16
Adam Menczykowski <adammenczykowski@gmail.com>
2018-02-15 09:53:05 +0000

    Initial commit

---
+++ src/controllers/FeedController.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Instagram Api plugin for Craft CMS 3.x
+ *
+ * Instagram Api guzzle endpoint plugin for JS to consume as AJAX
+ *
+ * @link      https://youi.design
+ * @copyright Copyright (c) 2018 Adam Menczykowski
+ */
+
+namespace youi\instagramapi\controllers;
+
+use Craft;
+use craft\web\Controller;
+
+/**
+ * Feed Controller
+ *
+ *
+ * @author    Adam Menczykowski
+ * @package   InstagramApi
+ * @since     1
+ */
+class FeedController extends Controller
+{
+
+    /**
+     * @access protected
+     */
+    protected $allowAnonymous = ['index'];
+
+    /**
+     * Handle a request going to our plugin's index action URL,
+     * e.g.: actions/instagram-api/feed
+     *
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $client = new \GuzzleHttp\Client;
+
+        $response = $client->get('https://api.instagram.com/v1/users/self/media/recent', [
+            'query' => [
+                'access_token' => getenv("INSTAGRAM_ACCESS_TOKEN"),
+                'count' => 4
+            ]
+        ]);
+        try {
+            $data = $response->getBody();
+            Craft::info($data);
+            return $data;
+        } catch (\yii\base\Exception $e) {
+            return $e;
+        }
+
+
+    }
+
+}
---
+++ src/translations/en/instagram-api.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Instagram Api plugin for Craft CMS 3.x
+ *
+ * Instagram Api guzzle endpoint plugin for JS to consume as AJAX
+ *
+ * @link      https://youi.design
+ * @copyright Copyright (c) 2018 Adam Menczykowski
+ */
+
+/**
+ * Instagram Api en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('instagram-api', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    Adam Menczykowski
+ * @package   InstagramApi
+ * @since     1
+ */
+return [
+    'Instagram Api plugin loaded' => 'Instagram Api plugin loaded',
+];
